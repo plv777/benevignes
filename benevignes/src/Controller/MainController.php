936408b662212ddb81de8ca87d243eb8aecee206
= <=>
2018-06-19 12:41:24 +0200

    Starter Menu (Phase 071)

--- benevignes/src/Controller/MainController.php
+++ benevignes/src/Controller/MainController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response ;
 use Symfony\Component\HttpFoundation\Request ;
 use Symfony\Component\Routing\Annotation\Route ;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller ;
-// pour créer erreur 403 :
+
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException ;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException ;
 use Symfony\Component\HttpKernel\Exception\HttpException ;
